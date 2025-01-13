1b40fc9c60e4c1464b6f17b297630cf12eb56953
funnyboy-roks <funnyboyroks@gmail.com>
2025-01-13 00:18:00 -0600

    Initial Commit

---
+++ tests/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static void main(String... args) {
+        var scanner = new java.util.Scanner(System.in);
+        var line = scanner.nextLine();
+        if (line.contains("w")) { // Invalid output
+            System.out.println(line);
+        } else if (line.contains("2")) { // Timeout
+            while (true);
+        } else { // correct output
+            char[] chars = line.toCharArray();
+            for (int i = 0; i < chars.length / 2; ++i) {
+                char t = chars[i];
+                chars[i] = chars[chars.length - i - 1];
+                chars[chars.length - i - 1] = t;
+            }
+            System.out.println(new String(chars));
+        }
+        scanner.close();
+    }
+}
---
+++ tests/java.rs
@@ -0,0 +1,64 @@
+use std::time::Duration;
+
+use erudite::{RunOutput, Runner, SimpleOutput, TestFailReason, TestOutput};
+
+#[tokio::test]
+async fn java_success() -> anyhow::Result<()> {
+    let mut runner = Runner::new();
+    runner
+        .compile_command(["javac", "Solution.java"])
+        .run_command(["java", "Solution"])
+        .test("hello", "olleh")
+        .test("hello world", "dlrow olleh")
+        .test("foo bar 2", "2 rab oof")
+        .create_file("Solution.java", include_str!("./Solution.java"))
+        .timeout(Duration::from_millis(1000));
+    dbg!(&runner);
+
+    let results = runner.run().await?;
+
+    dbg!(&results);
+
+    assert_eq!(
+        results,
+        RunOutput::RunSuccess(vec![
+            TestOutput::Pass,
+            TestOutput::Fail(TestFailReason::IncorrectOutput(SimpleOutput {
+                stdout: "hello world\n".to_string().into(),
+                stderr: String::new().into(),
+                status: 0
+            })),
+            TestOutput::Fail(TestFailReason::Timeout)
+        ])
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn java_compile_fail() -> anyhow::Result<()> {
+    let mut runner = Runner::new();
+    runner
+        .compile_command(["javac", "Solution404.java"])
+        .run_command(["java", "Solution"])
+        .test("hello", "olleh")
+        .test("hello world", "dlrow olleh")
+        .test("foo bar 2", "2 rab oof")
+        .create_file("Solution.java", include_str!("./Solution.java"))
+        .timeout(Duration::from_millis(1000));
+    dbg!(&runner);
+
+    let results = runner.run().await?;
+
+    dbg!(&results);
+
+    assert!(matches!(results, RunOutput::CompileFail(_)));
+
+    let RunOutput::CompileFail(err) = results else {
+        unreachable!()
+    };
+
+    assert_eq!(2, err.status, "Incorrect status code (from javac)");
+
+    Ok(())
+}
